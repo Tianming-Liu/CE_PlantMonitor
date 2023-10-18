64c1f3aaa8107f478da4f121688ff9f07e59881c
Tianming Liu <ucfntl0@ucl.ac.uk>
2023-10-18 18:55:20 +0100

    18 OCT

---
+++ Get_The_Time/sketch_oct18b.ino
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+#include <ezTime.h>
+
+const char* ssid     = "CE-Hub-Student";
+const char* password = "casa-ce-gagarin-public-service";
+
+Timezone GB;
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(100);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  waitForSync();
+
+  Serial.println("UTC: " + UTC.dateTime());
+
+  GB.setLocation("Europe/London");
+  Serial.println("London time: " + GB.dateTime()); 
+
+
+}
+
+void loop() {
+  delay(1000);
+  Serial.println(UTC.dateTime("l, d-M-y H:i:s.v T")); // UTC.dateTime("l, d-M-y H:i:s.v T")
+
+}
